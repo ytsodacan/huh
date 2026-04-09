08d046af88fccfe8911b6986ecdfc673b7c0dd76
Soda Can <seb1plazgaming@gmail.com>
2026-04-09 17:01:59 +1000

    sadasd

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1775717548|895830615';
+const CACHE_VERSION = '1775718111|1458495935';
 /** @type {string} */
 const CACHE_PREFIX = 'caine-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
